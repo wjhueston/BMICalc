30ed09773910bc0445653fdf49c59e1ef31a8e5e
Stacy Read <stacy.read.007@gmail.com>
2020-11-08 20:55:15 -0600

    Cleaned up project, removed manual validation

---
+++ validation.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    // Object containing the validation rules
+    var myRules =
+        {};
+
+    // Object containing the error messages
+    var myMessages =
+        {};
+
+    // Pass the configuration to the form's validate() method
+    // Needs submitHandler, rules, and messages properties
+    $("form").validate(
+        {}
+    );
+
+    function runMyProgram() {
+        // Change the text of the <p> with ID of "message" to
+        // state, for example, "You have registered Jane for grade 6 camp!"
+        // Use the name and grade the user provided in the form.
+
+
+    }
+
+
+
+});
